b76f0ef7b8d4d97aea50f8b7c20b0da0e0622340
munka <munka@DESKTOP-9OUH2LE>
2016-10-31 19:05:38 +0100

    Kivettem egy a betüt

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloka");
+            Console.WriteLine("Hellok");
             Console.ReadKey();
         }
     }
